487542fa0ccf6f6ad7fd7813ef2ad8572c64862c
Mohamed Sihan <39284618+Mshihan@users.noreply.github.com>
2021-09-22 18:12:21 +0530

    created server

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require("http");
+
+///////////////
+// SERVER
+const server = http.createServer((req, res) => {
+  res.end("Hello from the web server.....!!");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Opening the web server...");
+});
