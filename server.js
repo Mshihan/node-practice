1697ffda8c8337ea489b8a21f492ecc8c454ee78
Mohamed Sihan <39284618+Mshihan@users.noreply.github.com>
2021-09-22 22:10:16 +0530

    routing implemented using request.url

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const http = require("http");
+// const url = require("url");
 
 ///////////////
 // SERVER
 const server = http.createServer((req, res) => {
-  res.end("Hello from the web server.....!!");
+  if (req.url === "/" || req.url === "/overview") {
+    res.end("This is landing OVERVIEW page");
+  } else if (req.url === "/product") {
+    res.end("This is PRODUCT page");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+    });
+    res.end("<h1>This page is not exist</h1>");
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
